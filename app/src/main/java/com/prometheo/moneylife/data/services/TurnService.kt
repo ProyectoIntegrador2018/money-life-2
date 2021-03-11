668828adbfa498800c88d33813b307bccbe0eadf
the-vala <diegoi.vala@gmail.com>
2021-03-11 16:43:25 -0600

    Added missing turn services

--- app/src/main/java/com/prometheo/moneylife/data/services/TurnService.kt
+++ app/src/main/java/com/prometheo/moneylife/data/services/TurnService.kt
@@ -1,9 +1,6 @@
 package com.prometheo.moneylife.data.services
 
-import com.prometheo.moneylife.data.models.Turn
-import com.prometheo.moneylife.data.models.TurnAction
-import com.prometheo.moneylife.data.models.TurnActionBody
-import com.prometheo.moneylife.data.models.UserIdBody
+import com.prometheo.moneylife.data.models.*
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -16,4 +13,11 @@ interface TurnService {
 
     @PUT("/pregunta/afectaPreguntas/")
     suspend fun selectTurnActions(@Body turnActionBody: TurnActionBody): Response<List<Turn>>
+
+    @POST("/turno/inicio/")
+    suspend fun init(@Body userIdBody: UserIdBody): Response<List<Turn>>
+
+    @POST("/turno/interemedio/")
+    suspend fun refresh(@Body userIdBody: UserIdBody): Response<List<Turn>>
+
 }
