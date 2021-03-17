0e9c8f9e4cb329368e6e9ce9c263554238cb36e6
Fernando Limón <fernando@kinedu.com>
2021-03-17 13:56:22 -0600

    Fix: Model Variables Renamed

--- app/src/main/java/com/prometheo/moneylife/data/models/Turn.kt
+++ app/src/main/java/com/prometheo/moneylife/data/models/Turn.kt
@@ -6,8 +6,8 @@ data class Turn(
         @field:Json(name = "id") val turnId: Int,
         @field:Json(name = "NumeroTurnos") val turnNumber: Int,
         @field:Json(name = "Felicidad") val happiness: Float,
-        @field:Json(name = "DineroEfectivo") val cash: Float,
+        @field:Json(name = "DineroEfectivo") val balance: Float,
         @field:Json(name = "Ingresos") val income: Float,
         @field:Json(name = "Egresos") val expenses: Float,
-        @field:Json(name = "Sueldo") val balance: Float
+        @field:Json(name = "Sueldo") val salary: Float
 )
