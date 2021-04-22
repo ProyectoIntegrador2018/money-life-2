c09cf269bc32d98999c9021fa6b97ba2a4e9c436
the-vala <diegoi.vala@gmail.com>
2021-04-21 20:44:59 -0500

    News vm implementation

--- app/src/main/java/com/prometheo/moneylife/ui/news/NewsViewModel.kt
+++ app/src/main/java/com/prometheo/moneylife/ui/news/NewsViewModel.kt
@@ -1,4 +1,39 @@
 package com.prometheo.moneylife.ui.news
 
-class NewsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prometheo.moneylife.data.models.TurnEvent
+import com.prometheo.moneylife.data.models.UserIdBody
+import com.prometheo.moneylife.data.preferences.Prefs
+import com.prometheo.moneylife.data.room.AppDatabase
+import com.prometheo.moneylife.data.services.TurnService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel @Inject constructor (
+    private val turnService: TurnService,
+    private val prefs: Prefs,
+    private val db: AppDatabase
+) : ViewModel() {
+
+    val turnEvents: LiveData<List<TurnEvent>> = db.turnEventDao().observeAll()
+
+    //TODO: Mover a TurnViewModel?
+    fun updateTurnEvents () {
+        viewModelScope.launch {
+            try {
+                val response = turnService.getTurnEvents( UserIdBody( prefs.userId ) )
+                if ( response.isSuccessful ) {
+                    db.turnEventDao().insert( response.body()?.first()!! )
+                }
+            } catch ( err: Throwable ) {
+                //TODO: Add error message
+            }
+        }
+    }
+
 }
