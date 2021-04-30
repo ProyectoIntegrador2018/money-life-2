61abf59b2b4455c5a1a1653cd0511d19b13e4ffc
cicc98 <cicc1998@gmail.com>
2021-04-29 19:48:21 -0500

    Change TurnActionsDialog from Dialog to BottomSheet

--- app/src/main/java/com/prometheo/moneylife/ui/turn/TurnActionsDialogFragment.kt
+++ app/src/main/java/com/prometheo/moneylife/ui/turn/TurnActionsDialogFragment.kt
@@ -1,13 +1,11 @@
 package com.prometheo.moneylife.ui.turn
 
-import android.app.AlertDialog
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.prometheo.moneylife.data.models.TurnAction
 import com.prometheo.moneylife.data.models.TurnActionCategory
 import com.prometheo.moneylife.databinding.DialogTurnActionBinding
@@ -17,26 +15,19 @@ import com.xwray.groupie.GroupieViewHolder
 class TurnActionsDialogFragment(
     private val turnActions: List<TurnAction>,
     private val onItemClickListener: (Int, TurnActionCategory) -> Unit
-) : DialogFragment() {
+) : BottomSheetDialogFragment() {
 
     private var _binding: DialogTurnActionBinding? = null
     private val binding get() = _binding!!
 
     private val adapter = GroupAdapter<GroupieViewHolder>()
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        _binding = DialogTurnActionBinding.inflate(LayoutInflater.from(context))
-        return AlertDialog.Builder(requireActivity())
-            .setView(binding.root)
-            .create()
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = DialogTurnActionBinding.inflate(inflater)
         binding.rvTurnActions.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         binding.rvTurnActions.adapter = adapter
         adapter.update(turnActions.map { item ->
